ddd8cbeecb70aeb86f60511c8174c0be77bfc2e3
Ben Bancroft <bjbancroft19@gmail.com>
2018-02-11 20:10:59 +0000

    Created modal and header area

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,13 @@
 <template>
   <div id="app">
+    <div class="header container">
+      <h1>Fallout Terminal</h1>
+      <p id="show-modal" @click="showModal = true">What's this?</p>
+    </div>
+
+    <modal v-if="showModal" @close="showModal = false">
+      <h3 slot="header">The modal</h3>
+    </modal>
 
     <Terminal/>
   </div>
@@ -7,15 +15,35 @@
 
 <script>
 import Terminal from './components/Terminal';
+import Modal from './components/Modal';
 
 export default {
   name: 'App',
   components: {
-    Terminal
+    Terminal,
+    Modal
+  },
+  data() {
+    return {
+      showModal: false
+    };
   }
 };
 </script>
 
 <style lang="scss">
-
+.header {
+  padding: 10px 0;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  h1 {
+    font-size: 1.6em;
+    margin: 0;
+  }
+  p {
+    align-self: center;
+    cursor: pointer;
+  }
+}
 </style>
--- src/components/Terminal.vue
+++ src/components/Terminal.vue
@@ -545,7 +545,6 @@ $lightgreen: #4afa8f;
   background-repeat: repeat-y;
   opacity: .2;
   box-shadow: inset 0px 0px 1px 1px rgba(0, 0, 0, .8);
-  z-index: 999999;
   height: 100%;
   width: 100%;
   margin: -20px 0 0 -20px;
